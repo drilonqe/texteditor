1b84bec79d287e9ca60a4e81804ae00569a08dcf
qerimidr <qerimidr@132.161.196.86>
2025-02-23 14:19:08 -0600

    implemented methods insert and delete that move the cursor correctly when inserting or deleting something.

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -4,16 +4,31 @@ package edu.grinnell.csc207.texteditor;
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
+
     public String buffer;
     public int cursor;
-    
-    
+
+    public SimpleStringBuffer() {
+        this.buffer = "";
+        this.cursor = 0;
+    }
+
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        String beforeCursor = buffer.substring(0, cursor);
+        String afterCursor = buffer.substring(cursor);
+        buffer = beforeCursor + ch + afterCursor;
+        cursor++; // move cursor after inserting new character
     }
 
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (cursor > 0) {
+            String beforeCursor = buffer.substring(0, cursor - 1);
+            String afterCursor = buffer.substring(cursor);
+            
+           buffer = beforeCursor + afterCursor;
+            cursor--;
+        }
+
     }
 
     public int getCursorPosition() {
